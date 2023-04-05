b308659d551c284236b995b81d2f73c2496d6762
Brennan Ngo(Anh Ngo) <88129967+brennan992k@users.noreply.github.com>
2023-04-05 15:07:38 +0700

    update huawei ads

--- Assets/FruitsMania/Scripts/AdsEvents/AdsManager.cs
+++ Assets/FruitsMania/Scripts/AdsEvents/AdsManager.cs
@@ -1,17 +1,10 @@
-#if CHARTBOOST_ADS
-using ChartboostSDK;
-#endif
+using System.Diagnostics;
 using System;
 using System.Collections.Generic;
 using dotmob.Scripts.Core;
+using HmsPlugin;
 using UnityEngine;
-#if GOOGLE_MOBILE_ADS
-using GoogleMobileAds.Api;
-
-#endif
-#if UNITY_ADS
-using UnityEngine.Advertisements;
-#endif
+using HuaweiMobileServices.Ads;
 namespace dotmob.Scripts.AdsEvents
 {
     /// <summary>
@@ -31,386 +24,145 @@ namespace dotmob.Scripts.AdsEvents
         //rewarded zone for Unity ads
         public string rewardedVideoZone;
         //admob stuff
-#if GOOGLE_MOBILE_ADS
-        public InterstitialAd interstitial;
-        public BannerView banner;
-        private AdRequest requestAdmob;
-        public RewardBasedVideoAd admobRewardedVideo;
-#endif
-        private string admobUIDAndroid;
-        private string admobRewardedUIDAndroid;
-        private string admobUIDIOS;
-        private string admobRewardedUIDIOS;
-        private string admobBannerAndroid;
-        private string admobBannerIOS;
         private AdManagerScriptable adsSettings;
 
         private void Awake()
         {
             if (THIS == null) THIS = this;
-            else if(THIS != this)
+            else if (THIS != this)
             {
                 Destroy(gameObject);
                 return;
             }
             DontDestroyOnLoad(this);
-            adsSettings = Resources.Load<AdManagerScriptable>("Scriptable/AdManagerScriptable");
-            adsEvents = adsSettings.adsEvents;
-            //Interstitial
-            admobUIDAndroid = adsSettings.admobUIDAndroid;
-            admobUIDIOS = adsSettings.admobUIDIOS;
-
-            //Banner
-            admobBannerAndroid = adsSettings.admobBannerAndroid;
-            admobBannerIOS = adsSettings.admobBannerIOS;
-
-            //Reward
-            admobRewardedUIDAndroid = adsSettings.admobRewardedUIDAndroid;
-            admobRewardedUIDIOS = adsSettings.admobRewardedUIDIOS;
-
-#if UNITY_ADS//2.1.1
-            enableUnityAds = true;
-#else
-        enableUnityAds = false;
-#endif
-#if CHARTBOOST_ADS//1.6.1
-		enableChartboostAds = true;
-#else
-            enableChartboostAds = false;
-#endif
-#if GOOGLE_MOBILE_ADS
-		enableGoogleMobileAds =true;//1.6.1
-#if UNITY_ANDROID
-        MobileAds.Initialize(admobUIDAndroid);//2.1.6
-        interstitial = new InterstitialAd(admobUIDAndroid);
-#elif UNITY_IOS
-        MobileAds.Initialize(admobUIDIOS);//2.1.6
-        interstitial = new InterstitialAd(admobUIDIOS);
-#else
-        MobileAds.Initialize(admobUIDAndroid);//2.1.6
-		interstitial = new InterstitialAd (admobUIDAndroid);
-#endif
-
-        // Create an empty ad request.
-        requestAdmob = new AdRequest.Builder()
-                 .AddTestDevice(AdRequest.TestDeviceSimulator)
-                .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
-                .AddKeyword("game")
-                .Build();
-        // Load the interstitial with the request.
-        interstitial.LoadAd(requestAdmob);
-        interstitial.OnAdLoaded += HandleInterstitialLoaded;
-        interstitial.OnAdFailedToLoad += HandleInterstitialFailedToLoad;
-
-
             RequestBanner();
-#else
-            enableGoogleMobileAds = false;//1.6.1
-#endif
         }
 
 
         private void Start()
         {
-#if GOOGLE_MOBILE_ADS
-            RequestRewardBasedVideo();
-            admobRewardedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
-            admobRewardedVideo.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
-            admobRewardedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
-            admobRewardedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
-#endif
-        }
+            HMSAdsKitManager.Instance.OnRewarded = OnRewarded;
+            HMSAdsKitManager.Instance.OnInterstitialAdClosed = OnInterstitialAdClosed;
+
+            HMSAdsKitManager.Instance.ConsentOnFail = OnConsentFail;
+            HMSAdsKitManager.Instance.ConsentOnSuccess = OnConsentSuccess;
+            HMSAdsKitManager.Instance.RequestConsentUpdate();
 
 
-#if GOOGLE_MOBILE_ADS
+            #region SetNonPersonalizedAd , SetRequestLocation
 
-        public void HandleInterstitialLoaded(object sender, EventArgs args)
-    {
-        print("HandleInterstitialLoaded event received.");
-    }
+            var builder = HwAds.RequestOptions.ToBuilder();
 
-    public void HandleInterstitialFailedToLoad(object sender, AdFailedToLoadEventArgs args)
-    {
-        print("HandleInterstitialFailedToLoad event received with message: " + args.Message);
-    }
-#endif
-
-
-    public void RequestBanner()
-        {
-
-#if UNITY_ANDROID
-            banner = new BannerView(admobBannerAndroid, AdSize.Banner, AdPosition.Bottom);
-#elif UNITY_IOS
-        banner = new BannerView(admobBannerIOS, AdSize.Banner, AdPosition.Bottom);
-#else
-		banner = new BannerView(admobBannerAndroid, AdSize.Banner, AdPosition.Bottom);
-#endif
-
-            requestAdmob = new AdRequest.Builder()
-                .AddTestDevice(AdRequest.TestDeviceSimulator)
-                .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
-                .AddKeyword("game")
+            builder
+                .SetConsent("tcfString")
+                .SetNonPersonalizedAd((int)NonPersonalizedAd.ALLOW_ALL)
                 .Build();
-            banner.LoadAd(requestAdmob);
+
+            bool requestLocation = true;
+            var requestOptions = builder.SetConsent("testConsent").SetRequestLocation(requestLocation).Build();
+
+            UnityEngine.Debug.Log($"RequestOptions NonPersonalizedAds:  {requestOptions.NonPersonalizedAd}");
+            UnityEngine.Debug.Log($"Consent: {requestOptions.Consent}");
+
+            #endregion
         }
 
-
-        public void ShowBanner()
+        private void OnConsentSuccess(ConsentStatus consentStatus, bool isNeedConsent, IList<AdProvider> adProviders)
         {
-
-            if (banner != null)
+            UnityEngine.Debug.Log($"[HMS] AdsDemoManager OnConsentSuccess consentStatus:{consentStatus} isNeedConsent:{isNeedConsent}");
+            foreach (var AdProvider in adProviders)
             {
-                banner.Show();
+                UnityEngine.Debug.Log($"[HMS] AdsDemoManager OnConsentSuccess adproviders: Id:{AdProvider.Id} Name:{AdProvider.Name} PrivacyPolicyUrl:{AdProvider.PrivacyPolicyUrl} ServiceArea:{AdProvider.ServiceArea}");
             }
         }
 
-        public void HideBanner()
+        private void OnConsentFail(string desc)
         {
-            if (banner != null)
+            UnityEngine.Debug.Log($"[HMS] AdsDemoManager OnConsentFail:{desc}");
+        }
+
+        public void RequestBanner()
+        {
+            UnityEngine.Debug.Log("[HMS] AdsDemoManager ShowBannerAd");
+            HMSAdsKitManager.Instance.ShowBannerAd();
+        }
+
+        public void HideBannerAd()
+        {
+            UnityEngine.Debug.Log("[HMS] AdsDemoManager HideBannerAd");
+
+            HMSAdsKitManager.Instance.HideBannerAd();
+        }
+
+        public void ShowRewardedAd()
+        {
+            UnityEngine.Debug.Log("[HMS] AdsDemoManager ShowRewardedAd");
+            HMSAdsKitManager.Instance.ShowRewardedAd();
+        }
+
+        public void ShowInterstitialAd()
+        {
+            UnityEngine.Debug.Log("[HMS] AdsDemoManager ShowInterstitialAd");
+            HMSAdsKitManager.Instance.ShowInterstitialAd();
+        }
+
+        public void ShowSplashImage()
+        {
+            UnityEngine.Debug.Log("[HMS] ShowSplashImage!");
+
+            HMSAdsKitManager.Instance.LoadSplashAd("testq6zq98hecj", SplashAd.SplashAdOrientation.PORTRAIT);
+        }
+
+        public void ShowSplashVideo()
+        {
+            UnityEngine.Debug.Log("[HMS] ShowSplashVideo!");
+
+            HMSAdsKitManager.Instance.LoadSplashAd("testd7c5cewoj6", SplashAd.SplashAdOrientation.PORTRAIT);
+        }
+
+        public void OnRewarded(Reward reward)
+        {
+            UnityEngine.Debug.Log("[HMS] AdsDemoManager rewarded!");
+        }
+
+        public void OnInterstitialAdClosed()
+        {
+            UnityEngine.Debug.Log("[HMS] AdsDemoManager interstitial ad closed");
+        }
+
+        public void CheckAdsEvents(GameState state)
+        {
+            foreach (var item in adsEvents)
             {
-                banner.Hide();
-            }
-        }
-
-
-        private void RequestRewardBasedVideo()
-        {
-#if UNITY_EDITOR
-            string adUnitId = "unused";
-#elif UNITY_ANDROID
-			string adUnitId = admobRewardedUIDAndroid;
-#elif UNITY_IOS
-			string adUnitId = admobRewardedUIDIOS;
-#else
-			string adUnitId = "unexpected_platform";
-#endif
-            admobRewardedVideo = RewardBasedVideoAd.Instance;
-            print("admob reward request " + adUnitId);
-            requestAdmob = new AdRequest.Builder()
-                .AddTestDevice(AdRequest.TestDeviceSimulator)
-                .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
-                .AddKeyword("game")
-                .Build();
-            admobRewardedVideo.LoadAd(requestAdmob, adUnitId);
-        }
-
-        public void ShowRewardBasedVideo()
-        {
-            if (this.admobRewardedVideo.IsLoaded())
-            {
-                this.admobRewardedVideo.Show();
-
-            }
-            else
-            {
-                MonoBehaviour.print("Reward based video ad is not ready yet");
-            }
-        }
-
-        private void HandleRewardBasedVideoClosed(object sender, EventArgs e)
-        {
-            RequestRewardBasedVideo();
-            MonoBehaviour.print("HandleRewardBasedVideoClosed event received");
-        }
-
-        private void HandleRewardBasedVideoRewarded(object sender, Reward e)
-        {
-            InitScript.Instance.ShowReward();
-            string type = e.Type;
-            double amount = e.Amount;
-            MonoBehaviour.print(
-                "HandleRewardBasedVideoRewarded event received for " + amount.ToString() + " " + type);
-        }
-
-        private void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs e)
-        {
-            print("On admob reward load failed " + e.Message);
-        }
-
-        private void HandleRewardBasedVideoLoaded(object sender, EventArgs e)
-        {
-            print("On admob reward loaded " + e);
-        }
-
-     
-
-
-        public bool GetRewardedUnityAdsReady()
-        {
-#if APPODEAL
-        return AppodealIntegration.THIS.IsRewardedLoaded();
-#endif
-#if UNITY_ADS
-
-            rewardedVideoZone = "rewardedVideo";
-            if (Advertisement.IsReady(rewardedVideoZone))
-            {
-                return true;
-            }
-            else
-            {
-                rewardedVideoZone = "rewardedVideoZone";
-                if (Advertisement.IsReady(rewardedVideoZone))
+                if (item.gameEvent == state)
                 {
-                    return true;
+                    item.calls++;
+                    if (item.calls % item.everyLevel == 0)
+                        ShowAdByType(item.adType);
                 }
             }
-#endif
-
-            return false;
         }
 
-        private void OnDisable()
+        public void ShowRewardBasedVideo() {
+            ShowSplashVideo();
+        }
+
+        void ShowAdByType(AdType adType)
         {
-#if GOOGLE_MOBILE_ADS
-            if (interstitial != null)
+            switch (adType)
             {
-                interstitial.OnAdLoaded -= HandleInterstitialLoaded;
-                interstitial.OnAdFailedToLoad -= HandleInterstitialFailedToLoad;
-            }
-
-#endif
-        }
-
-        public void ShowRewardedAds()
-    {
-#if APPODEAL
-        Debug.Log("show Rewarded ads video in " + LevelManager.THIS.gameStatus);
-
-        if (GetRewardedUnityAdsReady())
-        {
-            AppodealIntegration.THIS.ShowRewardedAds();
-        }
-        else{
-            #if UNITY_ADS
-            Advertisement.Show(rewardedVideoZone, new ShowOptions
-            {
-                resultCallback = result =>
-                {
-                    if (result == ShowResult.Finished)
-                    {
-                        InitScript.Instance.ShowReward();
-                    }
-                }
-            });
-            #endif
-        }
-#elif UNITY_ADS
-        Debug.Log("show Rewarded ads video in " + LevelManager.THIS.gameStatus);
-
-        if (GetRewardedUnityAdsReady())
-        {
-            #if UNITY_ADS
-            Advertisement.Show(rewardedVideoZone, new ShowOptions
-            {
-                resultCallback = result =>
-                {
-                    if (result == ShowResult.Finished)
-                    {
-                        InitScript.Instance.ShowReward();
-                    }
-                }
-            });
-            #endif
-        }
-//#elif GOOGLE_MOBILE_ADS//2.2
-//        bool stillShow = true;
-//#if UNITY_ADS
-//        stillShow = !GetRewardedUnityAdsReady ();
-//#endif
-//        if(stillShow)
-//        {
-//            Debug.Log("show Admob Rewarded ads video in " + LevelManager.THIS.gameStatus);
-//            RewAdmobManager.THIS.ShowRewardedAd(CheckRewardedAds);
-//        }
-#endif
-    }
-
-    public void CheckAdsEvents(GameState state)
-    {    
-        foreach (var item in adsEvents)
-        {
-            if (item.gameEvent == state)
-            {
-                item.calls++;  
-                if (item.calls % item.everyLevel == 0)
-                    ShowAdByType(item.adType);
+                case AdType.AdmobInterstitial:
+                    ShowInterstitialAd();
+                    break;
+                case AdType.ChartboostInterstitial:
+                    ShowInterstitialAd();
+                    break;
+                case AdType.UnityAdsVideo:
+                    ShowSplashVideo();
+                    break;
+                default:
+                    break;
             }
         }
-    }
-
-    void ShowAdByType(AdType adType)
-    {
-        if (adType == AdType.AdmobInterstitial && enableGoogleMobileAds)
-            ShowAds(false);
-        else if (adType == AdType.UnityAdsVideo && enableUnityAds)
-            ShowVideo();
-        else if (adType == AdType.ChartboostInterstitial && enableChartboostAds)
-            ShowAds(true);
-        
-    }
-
-    public void ShowVideo()
-    {  
-#if UNITY_ADS
-        Debug.Log("show Unity ads video in " + LevelManager.THIS.gameStatus);
-
-        if (Advertisement.IsReady("video"))
-        {
-            Advertisement.Show("video");
-        }
-        else
-        {
-            if (Advertisement.IsReady("defaultZone"))
-            {
-                Advertisement.Show("defaultZone");
-            }
-        }
-#endif
-    }
-
-
-    public void ShowAds(bool chartboost = true)
-    {
-        #if APPODEAL
-        if(AppodealIntegration.THIS.IsInterstitialLoaded())
-        {
-            Debug.Log("show  Interstitial in " + LevelManager.THIS.gameStatus);
-            AppodealIntegration.THIS.ShowInterstitial();
-        }
-        #endif
-        if (chartboost)
-        {
-#if CHARTBOOST_ADS
-            Debug.Log("show Chartboost Interstitial in " + LevelManager.THIS.gameStatus);
-
-            Chartboost.showInterstitial(CBLocation.Default);
-            Chartboost.cacheInterstitial(CBLocation.Default);
-#endif
-        }
-        else
-        {
-#if GOOGLE_MOBILE_ADS
-            Debug.Log("show admob Interstitial in " + LevelManager.THIS.gameStatus);
-            if (interstitial.IsLoaded())
-            {
-                interstitial.Show();
-#if UNITY_ANDROID
-                interstitial = new InterstitialAd(admobUIDAndroid);
-#elif UNITY_IOS
-                interstitial = new InterstitialAd(admobUIDIOS);
-#else
-				interstitial = new InterstitialAd (admobUIDAndroid);
-#endif
-
-                // Create an empty ad request.
-                requestAdmob = new AdRequest.Builder().Build();
-                // Load the interstitial with the request.
-                interstitial.LoadAd(requestAdmob);
-            }
-#endif
-        }
-    }
     }
 }
